bbaf6855631d131ec4d07fa0a8ef9f143b1ea52f
Arnelle Balane <arnellebalane@gmail.com>
2015-05-24 03:25:49 +0800

    enable custom syntax highlighter for myhl

--- assets/javascripts/myhl.js
+++ assets/javascripts/myhl.js
@@ -2,10 +2,32 @@ var editor = {
     textarea: $('textarea'),
     line_numbers: $('.line-numbers'),
     current_line: $('.current-line'),
+    highlighted: $('pre code'),
     initialize: function() {
+        hljs.registerLanguage('myhl', function(hljs) {
+            console.log(hljs);
+            return {
+                keywords: 'var use as read print begin end',
+                contains: [
+                    {
+                        className: 'subkeyword',
+                        begin: /\b(statements|vars)\b/
+                    },
+                    {
+                        className: 'operator',
+                        begin: /[+*%=-]|([^\/]\/(?!\/))/
+                    },
+                    hljs.QUOTE_STRING_MODE,
+                    hljs.C_LINE_COMMENT_MODE,
+                    hljs.C_NUMBER_MODE
+                ]
+            };
+        });23
+
         editor.textarea.on('input keydown keyup', function() {
             editor.update_line_numbers();
             editor.highlight_line();
+            editor.highlight_code();
         });
     },
     update_line_numbers: function() {
@@ -25,7 +47,12 @@ var editor = {
 
         var offset = editor.line_numbers.find('.current').offset().top;
         editor.current_line.offset({ top: offset });
+    },
+    highlight_code: function() {
+        editor.highlighted.text(editor.textarea.val());
+        hljs.highlightBlock(editor.highlighted[0]);
     }
 };
 
 editor.initialize();
+12
